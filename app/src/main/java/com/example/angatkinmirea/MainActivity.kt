6db53f9a47dad465e22b0a5443949563b1247301
Alexander <alexander.angatkin@gmail.com>
2025-11-10 13:13:41 +0300

    task 3

--- app/src/main/java/com/example/angatkinmirea/MainActivity.kt
+++ app/src/main/java/com/example/angatkinmirea/MainActivity.kt
@@ -6,22 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.wear.tooling.preview.devices.WearDevices
+import androidx.compose.ui.unit.sp
 import com.example.angatkinmirea.ui.theme.AngatkinMIREATheme
 
 class MainActivity : ComponentActivity() {
@@ -86,3 +93,36 @@ fun HelloPreviewRound() {
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeTextPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(R.string.compose_description),
+            color = Color.Green,
+            fontSize = 16.sp,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+        )
+
+        Text(
+            text = stringResource(R.string.compose_description),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+        )
+
+        Text(
+            text = stringResource(R.string.compose_description),
+            modifier = Modifier
+                .background(Color.Green)
+                .padding(top = 48.dp),
+            color = Color.Black,
+            fontSize = 24.sp,
+            textDecoration = TextDecoration.Underline
+        )
+    }
+}
+
